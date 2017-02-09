ade50e89328d7eee7d03b16d0e3dff3145bcfd0d
Ricardo Malafaia <rbm4@cin.ufpe.br>
2017-02-09 19:47:22 +0000

    restore workspace

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20170208195852) do
     t.string   "produtos"
     t.string   "postcode"
     t.string   "pagseguro"
+    t.string   "txid_blockchain"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "txid_blockchain"
   end
 
   create_table "produtos", force: :cascade do |t|
