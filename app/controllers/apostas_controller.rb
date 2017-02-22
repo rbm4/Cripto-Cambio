b0cba4935038f68db817b241392e058fd83ee69e
Ricardo Malafaia <rbm4@cin.ufpe.br>
2017-02-22 23:49:12 +0000

    require user lotery

--- app/controllers/apostas_controller.rb
+++ app/controllers/apostas_controller.rb
@@ -1,5 +1,6 @@
 class ApostasController < ApplicationController
     skip_before_action :verify_authenticity_token, :only => [:sorteio]
+    before_action :require_user
     
     def index
     end
